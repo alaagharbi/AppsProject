150497411430ff61caa4a251f9e6eec1c789bf0a
alaagharbi <ag.gharbi7@gmail.com>
2022-09-02 10:37:09 +0100

    second sept first commit

--- AppsProject/View/LogInController.swift
+++ AppsProject/View/LogInController.swift
@@ -6,14 +6,45 @@ class LogInController: UIViewController {
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var emailTF: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.lablet.adjustsFontSizeToFitWidth = true
-    }
+  
     
 
     @IBAction func logTo(_ sender: UIButton) {
+        self.signIn(onSuccess: {self.navToView()}){
+        (errorMessage) in
+        print(errorMessage)
+    }
     }
    
+    func onErrorFunction() {
+        print("error !!!!")
+    }
+    func navToView(){
+        print("user Success")
+        present(TabBarViewController(), animated: true)
+    }
+    func signIn(onSuccess: @escaping() -> Void, onError: @escaping(_ errorMessage: String) -> Void) {
+        //affichage du progressBar
+        UserApi().SignIn(email: self.emailTF.text!, password : self.passwordTF.text!, onSuccess: {
+            onSuccess()
+        })
+        {
+            (errorMessage) in
+            onError(errorMessage)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.lablet.adjustsFontSizeToFitWidth = true
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
 
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
+    }
+        @objc func keyboardWillShow(sender: NSNotification) {
+             self.view.frame.origin.y = -150 // Move view 150 points upward
+        }
+
+        @objc func keyboardWillHide(sender: NSNotification) {
+             self.view.frame.origin.y = 0 // Move view to original position
+        }
 }
---
+++ AppsProject/View/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  AppsProject
+//
+//  Created by LookiiMobile on 2/9/2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+
+}
